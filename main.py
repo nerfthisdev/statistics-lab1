46552a4e8c23a5184088deeefd38cfd1dd866f3e
hujim0 <david.grigorev.2015@mail.ru>
2024-12-09 23:13:24 +0300

    chore: tidy up code

--- main.py
+++ main.py
@@ -4,15 +4,6 @@ from nums import get_second_lab_nums
 from prob_plotting import *
 from sample_functions import *
 
-
-# nums = [-0.45, 1.42, 0.52, 0.66, -1.63, -1.70, -0.42,
-#         0.17, -1.18, 0.14, 1.62, -1.71, 0.43, -0.18,
-#         1.42, 0.69, -0.55, -0.70, -1.51, -0.68]
-# nums = [0.83, 0.73, -0.48, 0.00, -1.35, 1.59, 0.31, 0.17,
-#         0.59, -0.45, -0.03, 0.73, -0.59, -1.59, 0.38, 1.49, 0.14, -0.62, -1.59, 1.45]
-
-# nums = [-0.03, -0.59, 0.38, 0.73, -1.59, 1.49, 0.14, -1.59, -0.38, -0.62, 1.45, -1.49, -0.15, 0.06, 0.61, -0.05, 0.63, -1.59, 0.62, 1.56]
-# nums = [63.4, 64.5, 57.1, 51.7, 40.1, 37.7, 45.8, 54.9, 35.9, 31.0, 35.5, 19.2, 13.6, 31.4, 40.1]
 nums = get_second_lab_nums()
 
 # Необходимо определить следующие статистические
@@ -35,7 +26,7 @@ print("Оценка математического ожидания: {:.2f}"
     .format(expected_value))
 
 print(f"Доверительный интервал мат. ожидания с доверительной вероятностью ɣ={confidence_prob}",
-    get_confidence_interval_for_expvalue(
+    get_confidence_interval_for_expvalue_large_set_with_latex_steps(
         expected_value,
         len(nums),
         get_sample_standard_deviation_corrected(
@@ -44,16 +35,19 @@ print(f"Доверительный интервал мат. ожидания с 
         confidence_prob
         )
     )
+
+
 print("Стандартное отклонение {:.2f}"
     .format(get_expected_value_deviation(nums)))
+print("Дисперсия {:.4f}"
+    .format(get_sample_variance(nums, expected_value)))
 print("Оценка выборочного среднеквадратического отклонения: {:.4f}"
     .format(get_sample_standard_deviation(nums, expected_value)))
-sample_standard_deviation_corrected = get_sample_standard_deviation_corrected(nums, expected_value)
 print("Оценка выборочного с.к.о. (исправленная): {:.4f}"
-    .format(sample_standard_deviation_corrected))
-print(f"Доверительный интервал дисперсии с доверительной вероятностью ɣ={confidence_prob}",
+    .format(get_sample_standard_deviation_corrected(nums, expected_value)))
+print(f"Доверительный интервал с.к.о.с доверительной вероятностью ɣ=\n{confidence_prob}",
     get_confidence_interval_for_standard_deviation(
-        sample_standard_deviation_corrected,
+        get_sample_standard_deviation_corrected(nums, expected_value),
         len(nums),
         confidence_prob/2 + 0.5
         )
